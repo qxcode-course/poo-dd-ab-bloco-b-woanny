e70b62366f8d9c48c662f38adaff1109bc254f3f
ALANY <56726183+woanny@users.noreply.github.com>
2025-10-26 13:23:12 +0000

    Motoca

--- tasks/poo/motoca/py/draft.py
+++ tasks/poo/motoca/py/draft.py
@@ -1,1 +1,99 @@
- 
+class Pessoa:
+    def __init__(self, nome: str, idade: int):
+        self.__nome = nome
+        self.__idade = idade
+
+    def getNome(self):
+        return self.__nome
+    
+    def getIdade(self):
+        return self.__idade
+
+    def __str__(self):
+        return f'{self.__nome}:{self.__idade}'
+
+
+class Moto:
+    def __init__(self, potencia: int = 1):
+        self.__pessoa: Pessoa | None = None
+        self.__potencia = potencia
+        self.__time = 0
+
+    def inserir(self, pessoa: Pessoa):
+        if self.__pessoa != None:
+            print('fail: busy motorcycle')
+            return
+        self.__pessoa = pessoa
+
+    def remover(self):
+        if self.__pessoa == None:
+            print('fail: empty motorcycle')
+            return
+        aux = self.__pessoa
+        self.__pessoa = None
+        return aux
+
+    def buyTime(self, time: int):
+        self.__time += time
+        
+    def dirigir(self, time: int):
+        if self.__time == 0:
+            print('fail: buy time first')
+            return
+        if self.__pessoa == None:
+            print('fail: empty motorcycle')
+            return
+        if self.__pessoa.getIdade() > 10:
+            print('fail: too old to drive')
+            return
+        if time > self.__time:
+            print(f'fail: time finished after {self.__time} minutes')
+            self.__time = 0
+            return
+        self.__time -= time
+
+    def honk(self):
+        print("P" + ("e" * self.__potencia) + "m")
+
+    def init(self, potencia: int):
+        self.__potencia = potencia
+        self.__time = 0
+        self.__pessoa = None
+
+    def __str__(self):
+        pessoa_str = "(empty)" if self.__pessoa is None else f"({self.__pessoa})"
+        return f"power:{self.__potencia}, time:{self.__time}, person:{pessoa_str}"
+
+
+def main():
+    moto = Moto()
+    while True:
+        line = input()
+        print('$' + line)
+        args = line.split()
+
+        if args[0] == 'end':
+            break
+        elif args[0] == 'show':
+            print(moto)
+        elif args[0] == 'init':
+            moto.init(int(args[1]))
+        elif args[0] == 'enter':
+            nome = str(args[1])
+            idade = int(args[2])
+            moto.inserir(Pessoa(nome, idade))
+        elif args[0] == 'leave':
+            pessoa = moto.remover()
+            if pessoa != None:
+                print(pessoa)
+        elif args[0] == 'buy':
+            moto.buyTime(int(args[1]))
+        elif args[0] == 'drive':
+            moto.dirigir(int(args[1]))
+        elif args[0] == 'honk':
+            moto.honk()
+        else:
+            print('error')
+
+
+main()
--- tasks/poo/notebook/py/draft.py
+++ tasks/poo/notebook/py/draft.py
@@ -2,16 +2,26 @@ class Notebook:
     def __init__(self):
         self.__ligado: bool = False #True = Ligado False = Desligado
         self.__bateria: Bateria | None = None
+        self.__carregador: Carregador | None = None
 
-    def ligar(self):
-        if self.__bateria is None or self.__bateria.getCarga() == 0:
-            print('não foi possível ligar')
+    def mostrar(self):
+        status = 'Ligado' if self.__ligado else 'Desligado'
         
-        if not self.__ligado: # se não for falso é verdadeiro e verdadeiro é ligado
-            self.__ligado = True
-            print('notebook ligado')
-        else:
+        bateria_str = 'Nenhuma' if self.__bateria == None else f'({self.__bateria.getCarga()}/{self.__bateria.getCapacidade()})'
+        carregador_str = 'Desconhecido' if self.__carregador == None else f'(Potência {self.__carregador.getPotencia()})'
+        print(f'Status: {status}, Bateria: {bateria_str}, Carregador: {carregador_str}')
+    
+    def ligar(self):
+        if self.__ligado:
             print('notebook já está ligado')
+            return
+
+        if (self.__bateria == None or self.__bateria.getCarga() == 0) and self.__carregador == None:
+            print('não foi possível ligar')
+            return
+
+        self.__ligado = True
+        print('notebook ligado')
 
     def desligar(self):
         if self.__ligado: # se for falso é falso e falso é desligado
@@ -20,21 +30,12 @@ class Notebook:
         else:
             print ('notebook já está desligado')
 
-    def mostrar(self):
-        status = 'Ligado' if self.__ligado else 'Desligado'
-        
-        if self.__bateria is None:
-            print(f'Status: {status}, Bateria: Nenhuma')
-        else:
-            print(f'Status: {status}, Bateria: ({self.__bateria.getCarga()}/{self.__bateria.getCapacidade()})')
-
     def usar(self, tempo):
+        if self.__carregador != None and self.__bateria != None:
+            return self.__carregador.carregar()
+
         if not self.__ligado:
             print('erro: ligue o notebook primeiro')
-            return
-        
-        if self.__bateria is None:
-            print('não foi possível ligar')
             return
 
         tempo_usado = self.__bateria.consumir(tempo)
@@ -48,14 +49,28 @@ class Notebook:
         self.__bateria = bateria
 
     def rmBateria(self):
-        if self.__bateria is None:
+        if self.__bateria == None:
             print('já está sem bateria')
             return None
         else:
             print('bateria removida')
+            self.__bateria.consumir(self.__bateria.getCarga())
             bateria = self.__bateria
             self.__bateria = None
             return bateria
+
+    def setCarregador(self, carregador):
+        self.__carregador = carregador
+
+    def rmCarregador(self):
+        if self.__carregador == None:
+            print('já está sem carregador')
+            return None
+        else:
+            print('carregador removido')
+            carregador = self.__carregador
+            self.__carregador = None
+            return carregador
 
 class Bateria:
     def __init__(self, capacidade):
@@ -79,23 +94,58 @@ class Bateria:
         else:
             self.__carga -= tempo # diminui o tempo na carga
             return tempo
+
+class Carregador:
+    def __init__(self, potencia):
+        self.__potencia: int = potencia
+        self.__bateria: Bateria | None = None
+
+    def mostrar(self):
+        print(f'Potência {self.__potencia}')
+
+    def getPotencia(self):
+        return self.__potencia
+
+    def carregar(self, tempo):
+        if self.__bateria != None:
+            carga_nova = self.__bateria.getCarga() + tempo * self.__potencia
+
+        if carga_nova > self.__bateria.getCapacidade():
+            carga_nova = self.__bateria.getCapacidade()
         
+        return carga_nova
+
+
 notebook = Notebook() # criando notebook
-notebook.mostrar()    # msg: Status: Desligado, Bateria: Nenhuma
-notebook.usar(10)     # msg: erro: ligue o notebook primeiro
+notebook.mostrar()    # msg: Status: Desligado, Bateria: Nenhuma, Carregador: Desconectado
 notebook.ligar()      # msg: não foi possível ligar
-notebook.mostrar()    # msg: Status: Desligado, Bateria: Nenhuma
+notebook.usar(10)     # msg: notebook desligado
+
 bateria = Bateria(50) # criando bateria que suporta 50 minutos e começa carregada
 bateria.mostrar()     # (50/50)
 notebook.setBateria(bateria) # coloca a bateria no notebook
-notebook.mostrar()    # msg: Status: Desligado, Bateria: (50/50)
-notebook.usar(10)     # msg: notebook desligado
-notebook.ligar()      # msg: notebook ligado
-notebook.mostrar()    # msg: Status: Ligado, Bateria: (50/50)
-notebook.usar(30)     # msb: Usando por 30 minutos
-notebook.mostrar()    # msg: Status: Ligado, Bateria: (20/50)
-notebook.usar(30)     # msb: Usando por 20 minutos, notebook descarregou
-notebook.mostrar()    # msg: Status: Desligado, Bateria: (0/50)
-notebook.ligar()      # msg: não foi possível ligar
+
+notebook.mostrar() # msg: Status: Desligado, Bateria: (50/50), Carregador: Desconectado
+notebook.ligar()   # msg: notebook ligado
+notebook.mostrar() # msg: Status: Ligado, Bateria: (50/50), Carregador: Desconectado
+notebook.usar(30)  # msb: Usando por 30 minutos
+notebook.mostrar() # msg: Status: Ligado, Bateria: (20/50), Carregador: Desconectado
+notebook.usar(30)  # msb: Usando por 20 minutos, notebook descarregou
+notebook.mostrar() # msg: Status: Desligado, Bateria: (0/50), Carregador: Desconectado
+
 bateria = notebook.rmBateria() # msg: bateria removida
-bateria.mostrar()     # (0/50)
+bateria.mostrar()  # (0/50)
+notebook.mostrar() # msg: Status: Desligado, Bateria: Nenhuma, Carregador: Desconectado
+
+carregador = Carregador(2) # criando carregador com 2 de potencia
+carregador.mostrar() # (Potência 2)
+
+notebook.setCarregador(carregador) # adicionando carregador no notebook
+notebook.mostrar() # msg: Status: Desligado, Bateria: Nenhuma, Carregador: (Potência 2)
+notebook.ligar()   # msg: notebook ligado
+notebook.mostrar() # msg: Status: Ligado, Bateria: Nenhuma, Carregador: (Potência 2)
+
+notebook.setBateria(bateria)
+notebook.mostrar() # msg: Status: Ligado, Bateria: (0/50), Carregador: (Potência 2)
+notebook.usar(10)  # msg: Notebook utilizado com sucesso
+notebook.mostrar() # msg: Status: Ligado, Bateria: (20/50), Carregador: (Potência 2)
